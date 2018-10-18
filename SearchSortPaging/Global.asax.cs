0165725e4c23d3b3193a76ee6f3e7e0ad89299b7
Rolf Thomassen <rolf.thomassen@hotmail.com>
2018-10-18 09:46:48 +0200

    Update

--- SearchSortPaging/Global.asax.cs
+++ SearchSortPaging/Global.asax.cs
@@ -16,6 +16,7 @@ namespace SearchSortPaging
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            MVCGridConfig.RegisterGrids();
         }
     }
 }
